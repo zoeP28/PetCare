b9a1d5245806d53ac94c49f0a758a4de0969c7d8
Ζωή <zoeP28>
2023-04-25 19:53:29 +0300

    database changes

--- petCare.sql
+++ petCare.sql
@@ -2,7 +2,9 @@ CREATE TABLE User (
   id INT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(20) NOT NULL
+  phone_number VARCHAR(20) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  profile_picture VARBINARY(255) NOT NULL
 );
 
 CREATE TABLE Pet (
@@ -15,58 +17,76 @@ CREATE TABLE Pet (
   FOREIGN KEY (user_id) REFERENCES User(id)
 );
 
-CREATE TABLE Review (
-  id INT PRIMARY KEY,
-  rating INT CHECK (rating > 0 and rating < 5) NOT NULL,
-  comment TEXT,
-  date DATETIME,
-  user_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES User(id)
-);
-
-CREATE TABLE Appointment (
-  id INT PRIMARY KEY,
-  date DATETIME,
-  service_type VARCHAR(255) NOT NULL,
-  pet_id INT NOT NULL,
-  user_id INT NOT NULL,
-  FOREIGN KEY (pet_id) REFERENCES Pet(id),
-  FOREIGN KEY (user_id) REFERENCES User(id)
-);
-
-CREATE TABLE Payment (
-  id INT PRIMARY KEY,
-  cost DECIMAL(10,2) NOT NULL,
-  method VARCHAR(255) NOT NULL,
-  date DATETIME,
-  appointment_id INT NOT NULL,
-  FOREIGN KEY (appointment_id) REFERENCES Appointment(id)
-);
 
 CREATE TABLE Business (
   id INT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   address VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(20) NOT NULL
+  phone_number VARCHAR(20) NOT NULL,
+  profile_picture VARBINARY(255) NOT NULL,
+  business_information TEXT NOT NULL
 );
+
 
 CREATE TABLE Service (
   id INT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   description TEXT,
   price DECIMAL(10,2) NOT NULL,
-  duration INT,
+  availability ENUM('yes','no') NOT NULL,
+  rating INT CHECK (rating > 0 and rating < 5) NOT NULL,
+  duration FLOAT,
   business_id INT NOT NULL,
   FOREIGN KEY (business_id) REFERENCES Business(id)
 );
 
-CREATE TABLE Business_for_Certification (
+CREATE TABLE Appointment (
   id INT PRIMARY KEY,
+  date DATETIME,
+  service_type VARCHAR(255) NOT NULL,
+  status ENUM('pending', 'canceled', 'confirmed'),
+  pet_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (pet_id) REFERENCES Pet(id),
+  FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+CREATE TABLE Rating (
+  id INT PRIMARY KEY,
+  rating INT CHECK (rating > 0 and rating < 5) NOT NULL,
+  comment TEXT,
+  date DATETIME,
+  user_id INT NOT NULL,
+  service_id INT NOT NULL,
+  Appointment_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (service_id) REFERENCES Service(id),
+  FOREIGN KEY (Appointment_id) REFERENCES Appointment(id)
+);
+
+
+CREATE TABLE Payment (
+  id INT PRIMARY KEY,
+  user_id INT NOT NULL,
+  cost DECIMAL(10,2) NOT NULL,
+  payment_method VARCHAR(255) NOT NULL,
+  date DATETIME,
+  appointment_id INT NOT NULL,
+  service_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (appointment_id) REFERENCES Appointment(id),
+  FOREIGN KEY (service_id) REFERENCES Service(id)
+);
+
+CREATE TABLE Business_for_Certification (
+  id INT PRIMARY KEY NOT NULL,
+  business_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   address VARCHAR(255) NOT NULL,
   phone_number VARCHAR(20) NOT NULL,
-  email VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  FOREIGN KEY (business_id) REFERENCES Business(id)
 );
 
 CREATE TABLE Certificates (
@@ -98,7 +118,9 @@ CREATE TABLE Admin (
   id INT PRIMARY KEY,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  profile_picture VARBINARY(255) NOT NULL
 );
 
 CREATE TABLE Statistics (
@@ -108,4 +130,9 @@ CREATE TABLE Statistics (
   date DATETIME,
   business_id INT NOT NULL,
   FOREIGN KEY (business_id) REFERENCES Business(id)
+);
+
+CREATE TABLE Frequent_Questions (
+  id INT PRIMARY KEY,
+  date_created date NOT NULL
 );
