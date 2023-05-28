b202333a76eac4499d94f0038b906a427d429f5c
Marialo2001 <marialo2001.ml@gmail.com>
2023-05-28 22:45:05 +0300

    Appointment

---
+++ PetCare/src/Appointment.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Appointment {
+    private Vet vet;
+    private String time;
+
+    public Appointment(Vet vet, String time) {
+        this.vet = vet;
+        this.time = time;
+    }
+
+    public Vet getVet() {
+        return vet;
+    }
