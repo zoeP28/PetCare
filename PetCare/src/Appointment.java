f512e7a51605673bbbb0b594746e6f2869daadcc
Marialo2001 <marialo2001.ml@gmail.com>
2023-05-28 23:01:18 +0300

    createAppoinment

--- PetCare/src/Appointment.java
+++ PetCare/src/Appointment.java
@@ -23,3 +23,18 @@ public class Appointment {
         //return "Appointment with " + vet.getName() + " at " + time;
         return "Appointment with " + getVet().getName() + " at " + getTime();
     }
+    static void createAppointment(Scanner scanner) {
+
+        System.out.println("Select Vet:");
+
+        List<Vet> availableVets = Vet.displayVets();
+
+        System.out.print("Enter Vet number: ");
+        int vetIndex = Integer.parseInt(scanner.nextLine()) - 1;
+        Vet vet = availableVets.get(vetIndex);
+
+        System.out.print("Enter desired appointment time (HH:MM): ");
+        String desiredTime = scanner.nextLine();
+        boolean isAvailable = BookingSystem.checkAvailability(vet, desiredTime);
+
+        String appointmentTime;
