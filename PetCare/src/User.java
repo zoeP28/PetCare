42ebd430b2689ff7630e7af41c7cdecfdeb16986
Marialo2001 <marialo2001.ml@gmail.com>
2023-05-28 22:31:14 +0300

    projectcreation2

---
+++ PetCare/src/User.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private String password;
+    private Location location;
+
+    private List<Pet> pets;
+    private static List<User> users;
+    private List<Appointment> appointments;
