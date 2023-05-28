a3d80b49cedb0d7e2845036d702d4b4ed329abde
Marialo2001 <marialo2001.ml@gmail.com>
2023-05-28 22:25:43 +0300

    projectcreation

--- PetCare/src/Main.java
+++ PetCare/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Helgnxgggfngfnfnxfgnxgfnfgfxfgncome!");
+        System.out.printf("Henlgnxgggfngfnfnxfgnxgfnfgfxfgncome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
