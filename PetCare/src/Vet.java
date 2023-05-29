5655131bdbcc5d4e0a6b796159e6dc9fc7257fe2
Ζωή <zoepanou55@gmail.com>
2023-05-29 22:07:43 +0300

    vet class complete

--- PetCare/src/Vet.java
+++ PetCare/src/Vet.java
@@ -39,4 +39,43 @@ public class Vet {
         this.availableAppointments.put(appointmentTime, false);
     }
 
+
+    public static List<Vet> displayVets() {
+        System.out.println("Available Vets:");
+        List<Vet> availableVets = vets.stream()
+                .filter(vet -> vet.getLocation().getName().equals(Main.currentUser.getLocation().getName())).toList();
+
+        for(int i = 0; i < availableVets.size(); i++) {
+            System.out.println((i + 1) + ". " + availableVets.get(i).getName());
+        }
+        return availableVets;
+    }
+
+
+    public List<String> getAvailableAppointments() {
+        return availableAppointments.entrySet().stream()
+                .filter(Map.Entry::getValue)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    static void initVets() {
+
+        vets = new ArrayList<>();
+        Vet vet1 = new Vet("Vet1",Location.Patras);
+        Vet vet2 = new Vet("Vet2",Location.Athens);
+        Vet vet3 = new Vet("Vet3",Location.Patras);
+
+        vet1.setAppointmentAvailability("09:00", true);
+        vet1.setAppointmentAvailability("10:00", true);
+        vet2.setAppointmentAvailability("11:00", true);
+        vet2.setAppointmentAvailability("14:00", true);
+        vet3.setAppointmentAvailability("13:00", true);
+        vet3.setAppointmentAvailability("18:00", true);
+
+        vets.add(vet1);
+        vets.add(vet2);
+        vets.add(vet3);
+    }
+
 }
