1257f52b8b19537e8cea63271255a2e74fb4e089
Ζωή <zoepanou55@gmail.com>
2023-05-30 01:00:08 +0300

    main updated

--- PetCare/src/Main.java
+++ PetCare/src/Main.java
@@ -1,17 +1,85 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
+
 public class Main {
+
+    static User currentUser;
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("einai dokimastiko");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        User.initUsers();
+        Vet.initVets();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("***** Welcome! *****\nPlease login to continue.");
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        User user = User.login(username, password);
+
+        if(user == null) {
+            System.out.println("Invalid username or password.");
+        } else {
+            System.out.println("Successful Login!");
+            String option = "";
+            while(!option.equals("4")) {
+                System.out.println("\n****** Home Page *******");
+                System.out.println("1. Book Appointment\n2. Check Appointments\n3. Pet Health Records\n4. Exit ");
+                System.out.print("Enter option: ");
+                option = scanner.nextLine();
+
+                switch(option) {
+                    case "1":
+                        Appointment.createAppointment(scanner);
+                        break;
+
+                    case "2":
+                        User.checkAppointments();
+                        break;
+
+                    case "3":
+                        // Pet Health Records Options
+                        System.out.println("Welcome to Pet Health Records");
+                        System.out.println("1. Add Pet");
+                        System.out.println("2. Select Pet");
+                        System.out.print("Enter your choice (1-2): ");
+                        int choice = new Scanner(System.in).nextInt();
+
+                        switch (choice) {
+                            case 1 -> {
+                                Pet newPet = Form.fillForm();
+                                user.addPet(newPet);
+                                System.out.println("Pet added successfully!");
+                                ConfirmationMessage.showConfirmationMessage("Do you want to confirm the insertion?");
+                            }
+                            case 2 -> {
+
+                                List<Pet> pets = user.getPets();
+                                for (int i = 0; i < pets.size(); i++) {
+                                    System.out.println((i + 1) + ". " + pets.get(i).getName());
+                                }
+                                System.out.println("Select a pet to view its health records");
+                                int petChoice = new Scanner(System.in).nextInt();
+                                Pet selectedPet = pets.get(petChoice - 1);
+                                System.out.println("Health records for " + selectedPet.getName() + ":");
+                                selectedPet.displayHealthRecord();
+
+                            }
+                            default -> System.out.println("Invalid choice");
+                        }
+                        break;
+
+                    case "4":
+                        System.out.println("Exiting, thank you!");
+                        break;
+
+                    default:
+                        System.out.println("Invalid option, please try again.");
+                        break;
+                }
+            }
         }
     }
 }
+
