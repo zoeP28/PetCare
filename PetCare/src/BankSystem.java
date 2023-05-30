e7c306d2873269316670138ba008344f8b9c15db
Antonia Karra <antoniakarra2@gmail.com>
2023-05-31 01:00:18 +0300

    BankSystem

--- PetCare/src/BankSystem.java
+++ PetCare/src/BankSystem.java
@@ -1,12 +1,35 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class BankSystem {
+    private Map<String, Double> accounts;
 
-    // In BankingSystem.java
-    public boolean validatePaymentInfo(Payment payment) {
-        // For simplicity, let's say that all payments are valid
-        System.out.println("Valid Transaction");
-        return true;
+    public BankSystem(){
+        accounts = new HashMap<>();
+    }
+
+    public void addBankAccount(String userId, double initialBalance){
+        accounts.put(userId, initialBalance);
+    }
+
+    public boolean processPayment(String userId, double amount){
+        Double currentBalance = accounts.get(userId);
+
+        if (currentBalance == null){
+            System.out.println("Error: No bank account found for user " + userId);
+            return false;
+        }
+
+        else if (currentBalance < amount){
+            System.out.println("Error: Not enough balance in user's account. Current balance: " + currentBalance);
+            return false;
+        }
+
+        else {
+            accounts.put(userId, currentBalance - amount);
+            System.out.println("Payment processed succesfully. New balance: " + (currentBalance - amount));
+            return true;
+        }
     }
 }
 
--- PetCare/src/ConfirmationMessage.java
+++ PetCare/src/ConfirmationMessage.java
@@ -9,6 +9,10 @@ public class ConfirmationMessage {
         return response.equalsIgnoreCase("yes");
     }
 
+    public static void displayConfirmation() {
+        System.out.println("Order confirmed! Thank you for your purchase.");
+    }
+
     public static void quantityError(Product product) {
         System.out.println("Sorry, we do not have enough stock of " + product.getName() + ".");
     }
