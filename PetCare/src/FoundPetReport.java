952f83574978aedd243e3d468b71d205f90c02f2
zoeP28 <zoepanou55@gmail.com>
2023-06-03 13:53:09 +0300

    1. found pet report as we did in lost pet report

---
+++ PetCare/src/FoundPetReport.java
@@ -0,0 +1,28 @@
+public class FoundPetReport extends Form {
+    public FoundPetReport(LostAndFoundPet pet, String contactInfo) {
+        super();
+        setDetails(pet, contactInfo);
+        pet.setStatus("Found");
+    }
+
+    @Override
+    public boolean validateDetails() {
+        if (getPet().getName().isEmpty() || getContactInfo().isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public Post createPost() {
+        if (!validateDetails()) {
+            System.out.println("Validation failed. Please fill out all required fields.");
+            return null;
+        }
+        Post post = new Post(this.getPet(), "Found", this.getContactInfo());
+        return post;
+    }
+
+
+}
