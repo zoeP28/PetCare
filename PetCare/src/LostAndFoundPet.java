1c271d7e703631fefad1bd248a9f8f145a762324
zoeP28 <zoepanou55@gmail.com>
2023-06-03 14:11:32 +0300

    menu creation lost initPets and select pets

--- PetCare/src/LostAndFoundPet.java
+++ PetCare/src/LostAndFoundPet.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LostAndFoundPet extends Pet {
     private String description;
     private String dateLostOrFound;
     private String status; // "Lost" or "Found"
+    private static List<LostAndFoundPet> lostPets = new ArrayList<>();
 
     public LostAndFoundPet(String name, int age, String breed, HealthRecord healthRecord, String lastSeenLocation,
                            String description, String dateLostOrFound, String status) {
@@ -26,5 +30,23 @@ public class LostAndFoundPet extends Pet {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    static void initLostPets() {
+        LostAndFoundPet pet1 = new LostAndFoundPet("Bobby", 3, "Golden Retriever", null,
+                "Park", "Bobby is a friendly dog.", "June 1, 2023", "Lost");
+        LostAndFoundPet pet2 = new LostAndFoundPet("Kitty", 2, "Persian Cat", null,
+                "Backyard", "Kitty is a fluffy cat.", "June 2, 2023", "Lost");
+        lostPets.add(pet1);
+        lostPets.add(pet2);
+    }
+
+
+    private static LostAndFoundPet selectLostPet(Scanner scanner) {
+        for (int i = 0; i < lostPets.size(); i++) {
+            System.out.println((i + 1) + ". " + lostPets.get(i));
+        }
+        int petIndex = scanner.nextInt() - 1;
+        return lostPets.get(petIndex);
     }
 }
